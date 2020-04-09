fc7af861dff7695873cf4c4ab071392e1775e098
H T <61760690+hassantariq216@users.noreply.github.com>
2020-04-09 14:21:43 -0400

    Add files via upload

--- A3-2/app.py
+++ A3-2/app.py
@@ -53,23 +53,23 @@ def authorize_login():
 
 	db=get_db()
 
-	#put data from db into dict form for iterations
 	db.row_factory = make_dicts 
 
-	# debug_arr=[]
 	student=[]
 	teacher=[]
 	for user in query_db('select * from student'):
 		if request.form['username']==list(user.values())[0] and request.form['password']==list(user.values())[1]:
 			session['logged_in']=True
 			session['user_type']='student'
+			session['user_id']=list(user.values())[0]
 			name_user = list(user.values())[2]
 
 	if not session.get('logged_in'):
 		for user2 in query_db('select * from teacher'):
 			if request.form['username']==list(user2.values())[0] and request.form['password']==list(user2.values())[1]:
 				session['logged_in']=True
-				session['user_type']='instructor'
+				session['user_type']='teacher'
+				session['user_id']=list(user2.values())[0]
 				name_user = list(user2.values())[2]
 		
 	if not session.get('logged_in'):
@@ -77,7 +77,7 @@ def authorize_login():
 
 	db.close()
 	session['user_name']=name_user
-	# return list(debug_arr[1].values())[1].__str__()
+
 	return home(name_user)
 
 @app.route('/home')
@@ -105,10 +105,8 @@ def register_menu():
 	name_us=request.form['name2']
 	type_user=request.form['User Type']
 
-	#put data from db into dict form for iterations
 	connection.row_factory = make_dicts 
 
-	# debug_arr=[]
 	student=[]
 	teacher=[]
 	for user in query_db('select * from student'):
@@ -131,8 +129,8 @@ def register_menu():
 			message="failure"
 		finally:
 			connection.close()
-			# return message
-			return render_template('login.html')
+			#return
+			return render_template('login.html', value="Successfully Registered!")
 
 	else:
 		try:
@@ -144,12 +142,107 @@ def register_menu():
 			message="failure"
 		finally:
 			connection.close()
-			# return message
-			return render_template('login.html')
+			#return
+			return render_template('login.html', value="Successfully Registered!")
+
+
+@app.route('/feedback', methods=['POST', 'GET'])
+def feedback_display():
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+		
+	if session['user_type']=='teacher':
+		db=get_db()
+		db.row_factory = make_dicts 
+		userid=session['user_id']
+		feedback=[]
+		for user in query_db('select * from feedback where t_id=?', [userid]):
+			feedback.append(user)
+		db.close()
+		return render_template('teacherFeedback.html', value=session['user_name'], user=feedback)
+
+	elif session['user_type']=='student':
+		db=get_db()
+		db.row_factory = make_dicts 
+		teacher=[]
+		for user4 in query_db('select * from teacher'):
+			teacher.append(user4)
+		db.close()
+		return render_template('studentFeedback.html', user4=teacher, value=session['user_name'])
+
+	else:
+		return render_template('login.html', value="Error: Please Log In")
+
+@app.route('/feedbackform', methods=['POST'])
+def feedback_form():
+	connection=get_db()
+	cursor=connection.cursor()
+	instructor_name=request.form['instructor option']
+	q1_response=request.form['q1']
+	q2_response=request.form['q2']
+	q3_response=request.form['q3']
+	q4_response=request.form['q4']
+	q5_response=request.form['q5']
+	instructor_id=''
+
+	connection.row_factory = make_dicts
+
+	for user3 in query_db('select * from teacher'):
+		if instructor_name==list(user3.values())[2]:
+			instructor_id=list(user3.values())[0]
+
+	try:
+		cursor.execute('INSERT INTO feedback (t_id, q1, q2, q3, q4, q5) VALUES (?,?,?,?,?,?)', (instructor_id, q1_response, q2_response, q3_response, q4_response, q5_response))
+		connection.commit()
+		message = "success"
+	except:
+		connection.rollback()
+		message="failure"
+	finally:
+		connection.close()
+		#return
+		return render_template('studentFeedback.html', value=session['user_name'], value2="Feedback sent!")
+
+
+@app.route('/coursematerial')
+def coursematerial():
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+	else:
+		return render_template('coursematerial.html', value=session['user_name'])
+
+@app.route('/assignments')
+def assignments():
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+	else:
+		return render_template('assignments.html', value=session['user_name'])
+
+@app.route('/calendar')
+def calendar():
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+	else:
+		return render_template('calendar.html', value=session['user_name'])
+
+@app.route('/courseteam')
+def courseteam():
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+	else:
+		return render_template('courseteam.html', value=session['user_name'])
+
+@app.route('/resources')
+def resources():
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+	else:
+		return render_template('resources.html', value=session['user_name'])
+
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True,port=5000)
 
 
 

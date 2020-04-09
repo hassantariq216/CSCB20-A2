b96cf65e9709907291fc23982845264fef9288b5
H T <61760690+hassantariq216@users.noreply.github.com>
2020-04-09 13:00:25 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
+from flask import Flask, redirect, render_template, request, session, abort, g
 import sqlite3
-
-from flask import Flask, render_template, request, session, g
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 
-DATAB = './database.db'
-
+DATAB='./assignment3.db'
 
 # Connecting database to flask
 # Source: https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
 def get_db():
-    db = getattr(g, '_database', None)
+	db=getattr(g, '_database', None)
 
-    if db is None:
-        db = g._database = sqlite3.connect(DATAB)
-    return db
-
+	if db is None:
+		db=g._database=sqlite3.connect(DATAB)
+	return db
 
 # get results from database through query
 # Source: https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
@@ -26,13 +23,11 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
-
-#
+# 
 # Source: https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
 def make_dicts(cursor, row):
     return dict((cursor.description[idx][0], value)
                 for idx, value in enumerate(row))
-
 
 # close connection to database
 # Source: https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
@@ -45,137 +40,215 @@ def close_connection(exception):
 
 @app.route('/', defaults={'name_user': ''})
 def home(name_user):
-    if not session.get('logged_in'):
-        if name_user == 'error':
-            return render_template('login.html', value="Error: Incorrect Username and/or Password")
-        else:
-            return render_template('login.html')
-    else:
-        return render_template('welcomeScreen.html', value=name_user)
-
+	if not session.get('logged_in'):
+		if name_user=='error':
+			return render_template('login.html', value="Error: Incorrect Username and/or Password")
+		else:
+			return render_template('login.html')
+	else:
+		return render_template('home.html', value=session['user_name'])
 
 @app.route('/login', methods=['POST'])
 def authorize_login():
-    db = get_db()
 
-    # put data from db into dict form for iterations
-    db.row_factory = make_dicts
+	db=get_db()
 
-    # debug_arr=[]
-    student = []
-    teacher = []
-    for user in query_db('select * from student'):
-        if request.form['username'] == list(user.values())[0] and request.form['password'] == list(user.values())[1]:
-            session['logged_in'] = True
-            name_user = list(user.values())[2]
+	db.row_factory = make_dicts 
 
-    if not session.get('logged_in'):
-        for user2 in query_db('select * from teacher'):
-            if request.form['username'] == list(user2.values())[0] and request.form['password'] == list(user2.values())[
-                1]:
-                session['logged_in'] = True
-                name_user = list(user2.values())[2]
+	student=[]
+	teacher=[]
+	for user in query_db('select * from student'):
+		if request.form['username']==list(user.values())[0] and request.form['password']==list(user.values())[1]:
+			session['logged_in']=True
+			session['user_type']='student'
+			session['user_id']=list(user.values())[0]
+			name_user = list(user.values())[2]
 
-    if not session.get('logged_in'):
-        name_user = 'error'
+	if not session.get('logged_in'):
+		for user2 in query_db('select * from teacher'):
+			if request.form['username']==list(user2.values())[0] and request.form['password']==list(user2.values())[1]:
+				session['logged_in']=True
+				session['user_type']='teacher'
+				session['user_id']=list(user2.values())[0]
+				name_user = list(user2.values())[2]
+		
+	if not session.get('logged_in'):
+		name_user='error'
 
-    db.close()
-    # return list(debug_arr[1].values())[1].__str__()
-    return home(name_user)
+	db.close()
+	session['user_name']=name_user
 
+	return home(name_user)
 
 @app.route('/home')
 def homepage():
-    return render_template('index.html')
-
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+	else:
+		return render_template('home.html', value=session['user_name'])
 
 @app.route('/register')
 def reg():
-    return render_template('registration.html')
+	return render_template('registration.html')
 
+@app.route('/logout')
+def logout():
+	session.clear()
+	return render_template('login.html')
 
 @app.route('/registerform', methods=['POST'])
 def register_menu():
-    connection = get_db()
-    cursor = connection.cursor()
-    usern = request.form['usn']
-    passw = request.form['psw']
-    name_us = request.form['name2']
-    type_user = request.form['User Type']
+	connection=get_db()
+	cursor=connection.cursor()
+	usern=request.form['usn']
+	passw=request.form['psw']
+	name_us=request.form['name2']
+	type_user=request.form['User Type']
 
-    if type_user == 'student':
-        try:
-            cursor.execute('INSERT INTO student (s_id,s_password, s_name) VALUES (?,?,?)', (usern, passw, name_us))
-            connection.commit()
-            message = "success"
-        except:
-            connection.rollback()
-            message = "failure"
-        finally:
-            connection.close()
-            # return message
-            return render_template('login.html')
+	connection.row_factory = make_dicts 
 
-    else:
-        try:
-            cursor.execute('INSERT INTO teacher (t_id,t_password, t_name) VALUES (?,?,?)', (usern, passw, name_us))
-            connection.commit()
-            message = "success"
-        except:
-            connection.rollback()
-            message = "failure"
-        finally:
-            connection.close()
-            # return message
-            return render_template('login.html')
+	student=[]
+	teacher=[]
+	for user in query_db('select * from student'):
+		if request.form['usn']==list(user.values())[0]:
+			connection.close()
+			return render_template('registration.html', value="Error: Username already exists!")
 
+	for user2 in query_db('select * from teacher'):
+		if request.form['usn']==list(user2.values())[0]:
+			connection.close()
+			return render_template('registration.html', value="Error: Username already exists!")
+	
+	if type_user=='student':
+		try:
+			cursor.execute('INSERT INTO student (s_id,s_password, s_name) VALUES (?,?,?)', (usern,passw, name_us))
+			connection.commit()
+			message = "success"
+		except:
+			connection.rollback()
+			message="failure"
+		finally:
+			connection.close()
+			#return
+			return render_template('login.html', value="Successfully Registered!")
 
-@app.route('/classgrades')
-def class_grades():
-    # TODO: get instructor id and check it is actually instructor
-    # if session['user_type'] == "instructor":
-
-    db = get_db()
-    db.row_factory = make_dicts
-    std_class = query_db('select * from grades natural join (select s_id, s_name from student)where t_id == '
-                         '"instructor2" ')
-    close_connection(db)
-    return render_template('teacher_grades.html', students=std_class)
-
-
-@app.route('/studentgrades')
-def student_grades():
-    # TODO: get student id and check it is actually instructor
-    # if session['user_type'] == "instructor":
-    db = get_db()
-    db.row_factory = make_dicts
-    grades = query_db('select * from grades')
-
-    db.close()
-    return render_template('student_grades.html', student=grades)
+	else:
+		try:
+			cursor.execute('INSERT INTO teacher (t_id,t_password, t_name) VALUES (?,?,?)', (usern,passw, name_us))
+			connection.commit()
+			message = "success"
+		except:
+			connection.rollback()
+			message="failure"
+		finally:
+			connection.close()
+			#return
+			return render_template('login.html', value="Successfully Registered!")
 
 
-@app.route('/studentremark', methods=['POST'])
-def remark_menu():
-    connection = get_db()
-    cursor = connection.cursor()
-    evaluation = request.form['eval']
-    remark = evaluation + '_remark'
-    reason_name = evaluation + '_reason'
-    reason = request.form['reason']
-    placeholder = 'student1'
+@app.route('/feedback', methods=['POST', 'GET'])
+def feedback_display():
+	if session['user_type']=='teacher' and session.get['logged_in']:
+		db=get_db()
+		db.row_factory = make_dicts 
+		userid=session['user_id']
+		feedback=[]
+		for user in query_db('select * from feedback where t_id=?', [userid]):
+			feedback.append(user)
+		db.close()
+		return render_template('teacherFeedback.html', value=session['user_name'], user=feedback)
 
-    try:
-        cursor.execute('UPDATE grades SET (%s)=1, (%s)=? WHERE s_id =? ' % (remark, reason_name), (reason, placeholder)) 
-        connection.commit()
-    except:
-        connection.rollback()
-    finally:
-        connection.row_factory = make_dicts
-        grades = query_db('select * from grades')
-        connection.close()
-        return render_template('student_grades.html', student=grades)
+	elif session['user_type']=='student' and session.get['logged_in']:
+		db=get_db()
+		db.row_factory = make_dicts 
+		teacher=[]
+		for user4 in query_db('select * from teacher'):
+			teacher.append(user4)
+		db.close()
+		return render_template('studentFeedback.html', user4=teacher, value=session['user_name'])
+
+	else:
+		return render_template('login.html', value="Error: Please Log In")
+
+@app.route('/feedbackform', methods=['POST'])
+def feedback_form():
+	connection=get_db()
+	cursor=connection.cursor()
+	instructor_name=request.form['instructor option']
+	q1_response=request.form['q1']
+	q2_response=request.form['q2']
+	q3_response=request.form['q3']
+	q4_response=request.form['q4']
+	q5_response=request.form['q5']
+	instructor_id=''
+
+	connection.row_factory = make_dicts
+
+	for user3 in query_db('select * from teacher'):
+		if instructor_name==list(user3.values())[2]:
+			instructor_id=list(user3.values())[0]
+
+	try:
+		cursor.execute('INSERT INTO feedback (t_id, q1, q2, q3, q4, q5) VALUES (?,?,?,?,?,?)', (instructor_id, q1_response, q2_response, q3_response, q4_response, q5_response))
+		connection.commit()
+		message = "success"
+	except:
+		connection.rollback()
+		message="failure"
+	finally:
+		connection.close()
+		#return
+		return render_template('studentFeedback.html', value=session['user_name'], value2="Feedback sent!")
+
+
+@app.route('/coursematerial')
+def coursematerial():
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+	else:
+		return render_template('coursematerial.html', value=session['user_name'])
+
+@app.route('/assignments')
+def assignments():
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+	else:
+		return render_template('assignments.html', value=session['user_name'])
+
+@app.route('/calendar')
+def calendar():
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+	else:
+		return render_template('calendar.html', value=session['user_name'])
+
+@app.route('/courseteam')
+def courseteam():
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+	else:
+		return render_template('courseteam.html', value=session['user_name'])
+
+@app.route('/resources')
+def resources():
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+	else:
+		return render_template('resources.html', value=session['user_name'])
+
+@app.route('/grades')
+def grades():
+	if not session.get('logged_in'):
+		return render_template('login.html', value="Error: Please Log In")
+	else:
+		return render_template('grades.html', value=session['user_name'])
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True,port=5000)
+
+
+
+
+
+
